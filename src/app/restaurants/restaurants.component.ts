0cfe43a8b98cdc60f1cf62325373c4502d9e419c
HUGO LEONEL DE CARVALHO <hlcarvalho.estagio@mp.mg.gov.br>
2018-05-08 16:04:11 -0300

    Commit teste

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantService } from './restaurants.service';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html',
+  styleUrls: ['./restaurants.component.css']
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[];
+
+  constructor(private restaurantsService: RestaurantService) { }
+
+  ngOnInit() {
+ //   this.restaurants = this.restaurantsService.restaurants();
+ this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+}
